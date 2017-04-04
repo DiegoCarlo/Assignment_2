dd2bdd3ffba6b26be61dfabe786b215353b2cb2b
DiegoCarlo <guerinidiegocarlo@gmail.com>
2017-04-04 16:07:49 +0200

    Completed

--- Assignment_2.py
+++ Assignment_2.py
@@ -6,9 +6,11 @@ from math import *
 from random import randint
 from random import random
 
-### window dimension ###
-windowX = 600
-windowY = 400
+### window param ###
+windowWidth = 1200
+windowHeight = 600
+windowX = 300
+windowY = 150
 
 ### mouse ###
 mouseX = 0
@@ -24,14 +26,41 @@ maxViewDistance = 150
 ### all object ###
 listSpaceObjects = None
 
+### param of visualization ###
+# Parameters for a more apreciable rappresentation
+coeffDiam = 0.45 # object diameter
+coeffDist = 10 # distance from the sun
+gapDist = 0 # 
+coeffRevo = 0.40 # coefficient of orbital time
+coeffRot = 0.35 # coefficient of rotation time
+coeffRevoSatellites = 10 # for moon
+subdivision = 60 # "faces of the object"
+
+### asteroids ###
+numberOfAsteroid = 200
+asteroidMarsGap = 50
+asteroidJupiterGap = 250
+
+### permits to draw objects ###
 quadric = None
 
+### manage the view ###
 camera = None
 
-update = True # run simulation
+### simulation variables ###
+update = True # do simulation or not
+simulationVelocity = 1
+maxSimulationVelocity = 50
 
 showOrbit = False
 
+allTexture = ["00_Sole.jpg", "01_Mercurio.jpg", "02_Venere.jpg", "03_Terra.jpg", \
+"03_01_Luna_2.jpg", "04_Marte.jpg", "05_Giove.jpg", "06_Saturno.jpg", "07_Urano.jpg", "08_Nettuno.jpg"]
+
+asteroidPath = "04.5_Asteroid_2.jpg"
+asteroidTexture = None
+
+textureSaturnRing = None
 ### The numerical values shown here have been taken from wikipedia ###
 ### These are the distances of the main objects from their center of revolution  (x * 10^9 meters) ###
 revolutionRadius = [0.00, 57.90, 108.20, 149.60, 0.384, 227.90, 778.30, 1427.00, 2871.00, 4497.10]
@@ -41,47 +70,55 @@ diameters = [1391.400, 4.878, 12.102, 12.756, 3.476, 6.786, 142.984, 120.536, 51
 revolutions = [0, 87.969, 224.700, 365.256, 27.321, 686.96, 4333.286, 10756.199, 30707.043, 60223.352]
 ### The durations of the bodies of rotation on its own axis (x hours) ###
 rotations = [654.6, 1403.667, 5816.083, 23.934, 653.930, 24.555, 9.897, 10.755, 17.192, 16.11] 
-
+### Inclination of its axis ###
 inclination = [0, 0.01, 177.36, 23.43, 1.54, 25.19, 3.13, 26.73, 97.77, 28.32]
+### Inclination to Sun's equator ###
+inclinationOrbit = [0, 3.38, 3.86, 7.155, 5.145, 5.65, 6.09, 5.51, 6.48, 6.43]
+
 #rotations = [26.5, 58.65, -243.01, 0.997, 27.32, 1.026, 0.41, 0.426, -0.746, 0.8]
 
 ### star, planet, satellite, environment ###
 allClasses = ["star", "planet", "planet", "planet", "satellite", "planet", "planet", "planet", "planet", "planet"]
 
-allTexture = ["00_Sole.jpg", "01_Mercurio.jpg", "02_Venere.jpg", "03_Terra.jpg", \
-"03_01_Luna_2.jpg", "04_Marte.jpg", "05_Giove.jpg", "06_Saturno.jpg", "07_Urano.jpg", "08_Nettuno.jpg"]
-
-textureSaturnRing = None
 
 class Camera(object):
-	''' this class manage the view. x, y and z are not strictly paid to opengl axes  '''
+	""" This class manage the view.
+
+	Attributes:
+		maxDistance: the max distance of the camera from the origin
+		minDistance: the minimum distance of the camera from the origin
+		
+		z: the current distance from the camera from the origin
+
+		xOrbit: the angle of vertical increment
+		yOrbit: the angle of horizontal increment
+
+		xOrbitTemp: the vertical angle increment while the mouse drags the view
+		yOrbitTemp: the horizontal angle increment while the mouse drags the view
+	"""
 	def __init__(self):
 
-		self.maxDistance = 50
+		self.maxDistance = 60
 		self.minDistance = 2.5
 
 		self.z = self.maxDistance * 0.75 # moving forwards -z moving backwards + z
 
-		self.xOrbit = 0 
-		self.yOrbit = 5
+		self.xOrbit = -65 
+		self.yOrbit = 0
 
 		self.xOrbitTemp = 0
 		self.yOrbitTemp = 0
 
-		
-
 		self.update()
 
 	def zoom(self, z):
-		''' zoom increment the distance from 0,0,0 '''
-
+		""" zoom increment the distance from origin """
 		temp = self.z + z
 		if temp > self.minDistance and temp < self.maxDistance:
 			self.z = temp
 
 	def update(self):
-		''' 
-		'''
+		""" aggiorna la posizione della telecamera """
 		gluLookAt(0, 0, self.z, \
 			0, 0, 0, \
 			0, 1, 0)
@@ -95,43 +132,65 @@ class Camera(object):
 		glRotate(self.yOrbit + self.yOrbitTemp, xComp, 0, zComp)
 
 	def incrementOrientation(self):
-		''' the function must be called when the mouse was released '''
+		""" the function must be called when the mouse was released """
 		self.xOrbit += self.xOrbitTemp
 		self.yOrbit += self.yOrbitTemp
 		self.xOrbitTemp = 0
 		self.yOrbitTemp = 0
 
 	def tempIncrementOrientation(self, x, y):
-		''' it must be called when the mouse drag the view '''
+		""" it must be called when the mouse drag the view """
 		self.xOrbitTemp = x
 		self.yOrbitTemp = y
 
 class Rotation(object):
+	""" this class is responsible for the rotation of an object """
 	def __init__(self, angPosition, angVelocity):
 		self.angPosition = angPosition
 		self.angVelocity = angVelocity
 
-	def update(self):
-		self.angPosition += self.angVelocity
+	def update(self, coeff):
+		self.angPosition += (self.angVelocity * coeff)
 		if self.angPosition >= 360:
 			self.angPosition -= 360
 
-class Revolution(Rotation):
-	def __init__(self, axisDistance, axis, angPosition, angVelocity, visible, idRootRevolution = None):
-		super(Revolution, self).__init__(angPosition, angVelocity)
+class Orbit(Rotation):
+	""" This class handles the orbit of the subject with respect to an axis
+		Attributes:
+			axisDistance: 
+			axis: orbit inclination to Sun's equator
+			angPosition: the angular position of the object
+			angVelocity: the angular velocity
+			visible: if True shows an orbital track
+			idRootorbit: an index of the listSpaceObjects and which object this orbit uses as center
+
+	"""
+	def __init__(self, axisDistance, axis, angPosition, angVelocity, visible, idRootOrbit = None):
+		super(Orbit, self).__init__(angPosition, angVelocity)
 		self.axisDistance = axisDistance
 		self.axis = axis
 		self.visible = visible
-		if idRootRevolution != None:
-			self.idRootRevolution = idRootRevolution
+		if idRootOrbit != None:
+			self.idRootOrbit = idRootOrbit
 		else:
-			self.idRootRevolution = None
+			self.idRootOrbit = None
 
-	def update(self):
-		super(Revolution, self).update()
+	def update(self, coeff):
+		super(Orbit, self).update(coeff)
 
 class SpaceObject(object):
+	"""	It contains the parameters of a space object
 
+	Attributes:
+		classes: star, planet, satellite, environment. To choose materials
+		orientation: self axis inclination
+		radius: SO radius
+		rotation: ...
+		revolution: orbit
+		pathImage: ...
+		subdivision: a higher value increases the number of faces of the sphere
+		texture: texture loaded
+	"""
 	def __init__(self, classes, orientation, radius, 
 		rotation, revolution, \
 		pathImage, subdivision):
@@ -148,57 +207,56 @@ class SpaceObject(object):
 
 		self.subdivision = subdivision ### sphere subdivision
 
-	def update(self):
-		self.rotation.update()
-		self.revolution.update()
+	def update(self, coeff):
+		self.rotation.update(coeff)
+		self.revolution.update(coeff)
 
 def initTexture(pathImage):
+	""" load texture for a single object """
 	global quadric
 	image = open(pathImage)
 
 	ix = image.size[0]
 	iy = image.size[1]
-
+	# load image using PIL
 	image_bytes = image.convert("RGBA").tobytes("raw", "RGBA", 0, -1)
+	# generate one texture name
 	texture = glGenTextures(1)
-
+	# bind a named texture to a texturing targhet
 	glBindTexture(GL_TEXTURE_2D, texture)
-
+	# build a two-dimensional mipmap
 	gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, ix, iy, GL_RGBA, GL_UNSIGNED_BYTE, image_bytes)
+	# parameters 
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT )
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT )
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR )
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR )
+	
+	# this texture must be generated for quadrics render
 	gluQuadricTexture(quadric, 1)
 
 	return texture
 
 def createSpaceObject():
-	global listSpaceObjects, maxViewDistance
-
-	coeffDiam = 0.45
-	coeffDist = 10
-	gapDist = 0
-	coeffRevo = 0.35
-	coeffRot = 0.5
-	coeffRevoSatellites = 10
-	subdivision = 60
+	""" This function instantiates the major planets """
+	global listSpaceObjects, maxViewDistance, coeffDiam, coeffDist, gapDist, coeffRevo, coeffRot, coeffRevoSatellites, subdivision
 
 	listSpaceObjects = []
-	### create all planet and moon ###
 	for i in range(0, len(diameters)):
 
+		# object rotation
 		rot = 360/rotations[i]
 		rotation = Rotation(0, rot * coeffRot)
 
+		# distance
 		dist = 0
 		if revolutionRadius[i] != 0:
 			dist = (log(revolutionRadius[i], 10) * coeffDist) - gapDist
 			#print log(revolutionRadius[i], 10)
-
+		# distance of the moon adjusted for consistency
 		if i == 4:
 			dist = 1
-
+		# revolution period
 		revo = 0
 		if revolutions[i] != 0:
 			revo = 1/log(revolutions[i], 10) * coeffRevo
@@ -206,40 +264,44 @@ def createSpaceObject():
 		if allClasses[i] == "satellite":
 			revo = revo * coeffRevoSatellites
 
-		revolution = Revolution(dist, [0,1,0],randint(0,360),revo, True)
-		radius = log(diameters[i]/2, 10) * coeffDiam
-		#print revo
+		# object orbit
+		revolution = Orbit(dist, inclinationOrbit[i], randint(0,360),revo, True)
+		radius = log(diameters[i], 10) * coeffDiam
+		
 		listSpaceObjects.append(SpaceObject(allClasses[i],[0,0,inclination[i]], radius, rotation, \
 			revolution, allTexture[i], subdivision))
 
-	### set mun as satellite ###
-	listSpaceObjects[4].revolution.idRootRevolution = 3
+	### set mun as satellite of the earth ###
+	listSpaceObjects[4].revolution.idRootOrbit = 3
 
-
+	# create some asteroids
 	createAsteroids(coeffDist, coeffDiam, gapDist, coeffRevo, 8)
 
+	# skysphere
 	rotation = Rotation(0, -0.02)
-	revolution = Revolution(0,[0,1,0],0,0, False)
+	revolution = Orbit(0,0,0,0, False)
 	listSpaceObjects.append(SpaceObject("environment",[0,0,45], maxViewDistance, rotation, \
 		revolution, "The_Milky_Way.jpg", subdivision))	
 
 def createAsteroids(coeffDist, coeffDiam, gapDist, coeffRevo, subdivision):
-	for i in range(0, 300):
+	""" this function create small asteroids """
+	global numberOfAsteroid, asteroidMarsGap, asteroidJupiterGap
+	for i in range(1, numberOfAsteroid):
 
 		rotation = Rotation(0, 0)
 		rand = random()
-		minDistance = revolutionRadius[5] + 50
-		maxDistance = revolutionRadius[6] - 250
+		minDistance = revolutionRadius[5] + asteroidMarsGap
+		maxDistance = revolutionRadius[6] - asteroidJupiterGap
 		gapDistance = maxDistance - minDistance
 		dist = rand * gapDistance + minDistance
 		dist = (log(dist, 10) * coeffDist) - gapDist
-		minRevolution = revolutions[5]
-		maxRevolution = revolutions[6]
-		gapRevolution = maxRevolution - minRevolution
-		revo = rand * gapRevolution + minRevolution
-		revolution = Revolution(dist, [0,1,0],randint(0,360), 1/log(revo, 10) * coeffRevo, False)
-		listSpaceObjects.append(SpaceObject("asteroid",[0,0,0], randint(10,40)*coeffDiam*0.0025, rotation, \
-		revolution, "04.5_Asteroid_2.jpg", subdivision)) 
+		minOrbit = revolutions[5]
+		maxOrbit = revolutions[6]
+		gapOrbit = maxOrbit - minOrbit
+		revo = rand * gapOrbit + minOrbit
+		revolution = Orbit(dist, randint(0,99)*0.02+5.8,randint(0,360), 1/log(revo, 10) * coeffRevo, False)
+		listSpaceObjects.append(SpaceObject("asteroid",[0,0,0], randint(10,40)*coeffDiam*0.0035, rotation, \
+		revolution, asteroidTexture, subdivision)) 
 
 def initScene():
 	global quadric, listSpaceObjects, camera, textureSaturnRing
@@ -251,9 +313,8 @@ def initScene():
 	glEnable(GL_COLOR_MATERIAL)
 	glEnable(GL_LIGHTING)
 	glEnable(GL_LIGHT0)
-	#glEnable(GL_NORMALIZE)
 
-	#Parameters for correct shading
+	# Parameters for correct shading
 	glShadeModel(GL_SMOOTH)
 	glDepthFunc(GL_LEQUAL)
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
@@ -263,8 +324,12 @@ def initScene():
 
 	quadric = gluNewQuadric()
 
+	asteroidTexture = initTexture(asteroidPath)
 	for i in listSpaceObjects:
-		i.texture = initTexture(i.pathImage)
+		if i.classes == "asteroid":
+			i.texture = asteroidTexture
+		else:
+			i.texture = initTexture(i.pathImage)
 
 	textureSaturnRing = initTexture("06_01_Anelli.jpg") #06_01_Anelli.jpg")
 
@@ -272,14 +337,18 @@ def initScene():
 
 def revolutionRoot(revolution):
 	global showOrbit
-	if revolution.idRootRevolution >= 0 and revolution.idRootRevolution < len(listSpaceObjects):
-		revolutionRoot(listSpaceObjects[revolution.idRootRevolution].revolution)
+	# if the object is orbiting around a second object which in turn is orbiting about a third object, run the rotation of the second object before and so recursively
+	# se l'oggetto orbita attorno ad un corpo che orbita a sua volta eseguo prima la rotazione del corpo e cosi ricorsivamente
+	if revolution.idRootOrbit >= 0 and revolution.idRootOrbit < len(listSpaceObjects):
+		revolutionRoot(listSpaceObjects[revolution.idRootOrbit].revolution)
 	
 	axis = revolution.axis
-
+	# he main bodies have their visible primary orbit, below them a track is represented
+	# i soggetti principali hanno la loro orbita primaria visibile, di seguito ne viene rappresentata una traccia
 	if showOrbit == True and revolution.visible == True:
 		glPushMatrix()
-		glRotate(revolution.angPosition, axis[0], axis[1], axis[2])
+		glRotate(revolution.axis,1,0,0)
+		glRotate(revolution.angPosition, 0,1,0)
 		glRotatef(90, 1, 0, 0)
 		glRotatef(135, 0, 0, 1)
 		fRadius = revolution.axisDistance #* 2 + (-item.radius)
@@ -300,18 +369,20 @@ def revolutionRoot(revolution):
 
 		glPopMatrix()
 
-	glRotate(revolution.angPosition, axis[0], axis[1], axis[2])
+	glRotate(revolution.axis,1,0,0)
+	glRotate(revolution.angPosition,0,1,0)
 	glTranslate(revolution.axisDistance, 0, 0)
-	glRotate(-revolution.angPosition, axis[0], axis[1], axis[2])
+	glRotate(-revolution.angPosition, 0,1,0)
 
 def drawSpaceObject(item):
 	global quadric, textureSaturnRing, update
 	
 	if update == True:
-		item.update()
+		item.update(simulationVelocity)
 	
 	glPushMatrix()
 
+	# choice of material
 	if item.classes == "planet" or item.classes == "environment" or item.classes == "asteroid":
 		glMaterialfv(GL_FRONT, GL_EMISSION, [.0, .0, .0, 1])
 		glMaterialfv(GL_FRONT, GL_SHININESS, 50)
@@ -325,18 +396,21 @@ def drawSpaceObject(item):
 		glMaterialfv(GL_FRONT, GL_SPECULAR, [.1, .1, .1, 1])
 
 	elif item.classes == "star":
-		glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, [.9, .9, .9, .0]) #with this, the sun EMITS light
+		glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, [1, 1, 1, .0]) #with this, the sun EMITS light
 		glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, 70)
 	
+	### positioning and rotations ###
 	revolutionRoot(item.revolution)
 
 	glEnable(GL_TEXTURE_2D)
 	glBindTexture(GL_TEXTURE_2D, item.texture)
+
 	glRotatef(-90, 1, .0, .0)
 	glRotatef(item.orientation[2], 0, 1, 0)
 
 	glRotatef(item.rotation.angPosition, .0, .0, 1)
 	
+	# skySphere
 	if item.classes == "environment":
 		glDepthMask(GL_FALSE)
 		gluQuadricOrientation(quadric, GLU_INSIDE)
@@ -350,6 +424,7 @@ def drawSpaceObject(item):
 	glDisable(GL_TEXTURE_2D)
 
 
+	### special guest: saturn ring ###
 	if item.pathImage == "06_Saturno.jpg":
 		glPushMatrix()
 		glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, [.0, .0, .0, 1])
@@ -372,17 +447,25 @@ def drawSpaceObject(item):
 		glPopMatrix()
 		glDisable(GL_TEXTURE_2D)
 
+def toggleShowOrbit():
+	global showOrbit
+	if showOrbit == True:
+		showOrbit = False
+	else:
+		showOrbit = True
+
 def drawScene():
 	global listSpaceObjects
 
 	glMatrixMode(GL_MODELVIEW)
 	glLoadIdentity()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	
 	camera.update()
 	glLightfv(GL_LIGHT0, GL_POSITION, [.0, .0, .0, 1])
+
 	for i in listSpaceObjects:
 		drawSpaceObject(i)
-
 	glutSwapBuffers()
 
 def resizeScene(width, height):
@@ -396,9 +479,43 @@ def resizeScene(width, height):
 	gluPerspective(60, float(width)/float(height), 0.1, maxViewDistance*2) # todo
 	glMatrixMode(GL_MODELVIEW)
 
+### interaction ###
 def keyPressed(key, x, y):
+	global simulationVelocity, maxSimulationVelocity
 	if key == chr(27):
 		sys.exit()
+
+	if key == '+' or key == 'l' or key == 'L':
+		if simulationVelocity < maxSimulationVelocity:
+			simulationVelocity += 1
+
+	if key == '-' or key == 'j' or key == 'J':
+		if simulationVelocity > - maxSimulationVelocity:
+			simulationVelocity -= 1 
+
+	if key == '0' or key == 'k' or key == 'K':
+		simulationVelocity = 1
+
+	if key == '5' or key == 'o' or key == 'O':
+		toggleShowOrbit()
+
+	if key == '4' or key == 'a' or key == 'A':
+		camera.xOrbit += 1
+
+	if key == '6' or key == 'd' or key == 'D':
+		camera.xOrbit -= 1
+
+	if key == '8' or key == 'w' or key == 'W':
+		camera.yOrbit += 1
+
+	if key == '2' or key == 's' or key == 'S':
+		camera.yOrbit -= 1
+
+	if key == '*' or key == 'x' or key == 'X':
+		camera.zoom(+0.5)
+
+	if key == '/' or key == 'z' or key == 'Z':
+		camera.zoom(-0.5)
 
 def mousePressed(button, state, x, y):
 	global mousePrevStateRight, mousePrevStateMiddle, mousePrevStateLeft, mouseTracking, mouseX, mouseY, camera, showOrbit, update
@@ -435,10 +552,7 @@ def mousePressed(button, state, x, y):
 
 	if button == 1:
 		if state == 0 and mousePrevStateMiddle == 1:
-			if showOrbit == True:
-				showOrbit = False
-			else:
-				showOrbit = True
+			toggleShowOrbit()
 	mousePrevStateMiddle = state
 
 def mouseMotion(x,y):
@@ -450,14 +564,14 @@ def mouseMotion(x,y):
 		camera.tempIncrementOrientation(xGap, yGap)
 
 def main():
-	global windowX, windowY, physicsThread
+	global windowWidth, windowHeight, physicsThread
 
 	glutInit()
-	glutInitWindowSize(windowX, windowY)
-	glutInitWindowPosition(100,100)
+	glutInitWindowSize(windowWidth, windowHeight)
+	glutInitWindowPosition(windowX, windowY)
 	glutCreateWindow("Assignment_2_Solar_System.py")
 	glutReshapeFunc(resizeScene)
-	glutFullScreen()
+	#glutFullScreen()
 	initScene()
 
 	glutDisplayFunc(drawScene)
@@ -470,5 +584,8 @@ def main():
 	glutMainLoop()
 
 if __name__ == "__main__":
-	print "Hit ESC key to quit."
+	print '\033[31m' + "Keyboard: (wasd zx lkj o)" + '\033[0m'
+	print '\033[32m' + "\nNumeric Keypad(8426 */ +0- 5)" + '\033[0m'
+	print '\033[34m' + "\nMouse(left, middle up, middle down, middle, left)" + '\033[0m'
+	print "\nHit ESC key to quit."
 	main()
